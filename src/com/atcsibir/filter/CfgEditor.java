80d2cadd47d0d66a10f0b42c8450769affea9e59
ifkin <wecan@list.ru>
2015-02-03 23:20:03 +0200

    cfg loader / editor

--- src/com/atcsibir/filter/CfgEditor.java
+++ src/com/atcsibir/filter/CfgEditor.java
@@ -7,22 +7,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class CfgEditor extends HttpServlet
 	{
+		private static final String cfgPath = System.getProperty("catalina.base") + "/conf/adpFilter/conf.json";
+
 		public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 			{
-
+				InputStream is = req.getInputStream(); // входящий пост с json'ом с параметрами
+				FileOutputStream fs = new FileOutputStream(new File(cfgPath));
+				IOUtils.copy(is,fs);
+				is.close();
+				fs.close();
+				resp.getWriter().print("Saved");
 			}
 
 		public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 			{
-				String currentCfg = IOUtils.toString(new FileInputStream(new File(System.getProperty("catalina.base") + "/conf/adpFilter/conf.json")), "UTF-8");;
-				PrintWriter rsp = resp.getWriter();
-				rsp.print("<html><head><title>Filter Cfg Editor</title><script type=\"text/javascript\" src=\"/radpf/js/jquery.js\"></script><script type=\"text/javascript\" src=\"/radpf/js/cfg.js\"></script></head><body><div id=\"currentCfg\"></div><div><input type=\"button\" value=\"Save\" id=\"saveCfg\" /> Status: <span id='status'></span></div><script type=\"text/javascript\">var currentCfg = " + currentCfg + ";</script></body></html>");
+				String currentCfg = IOUtils.toString(new FileInputStream(new File(cfgPath)), "UTF-8");
+				resp.getWriter().print("<html><head><title>Filter Cfg Editor</title><script type=\"text/javascript\" src=\"/radpf/js/jquery.js\"></script><script type=\"text/javascript\" src=\"/radpf/js/cfg.js\"></script><style type=\"text/css\">.ctrl{width:210px;margin-bottom:7px;}</style></head><body><div id=\"currentCfg\"></div><div><input type=\"button\" value=\"Save\" id=\"saveCfg\" /> Status: <span id='status'></span></div><script type=\"text/javascript\">var currentCfg = " + currentCfg + ";</script></body></html>");
 			}
 	}
--- web/js/cfg.js
+++ web/js/cfg.js
@@ -2,16 +2,16 @@ $(document).ready(function ()
     {
         for (key in currentCfg)
             {
-                $("#currentCfg").append("<input readonly size='35' type='text' value='" + key + "' id='" + key + "'/> ");
+                $("#currentCfg").append("<input readonly type='text' value='" + key + "' class='paramName ctrl'/> ");
 
                 if (currentCfg[key] == true || currentCfg[key] == false)
                     {
-                        $("#currentCfg").append("<select style='width:210px; margin-bottom: 7px;' id='" + key + "_value'><option value='true'>Enabled</option><option value='false'>Disabled</option></select><br/>");
+                        $("#currentCfg").append("<select class='ctrl' id='" + key + "_value'><option value='true'>Enabled</option><option value='false'>Disabled</option></select><br/>");
                         $("#" + key + "_value").val(currentCfg[key].toString());
                     }
                 else
                     {
-                        $("#currentCfg").append("<input style='width:210px; margin-bottom: 7px;' type='text' value='" + currentCfg[key] + "' id='" + key + "_value'/><br/>");
+                        $("#currentCfg").append("<input class='ctrl' type='text' value='" + currentCfg[key] + "' id='" + key + "_value'/><br/>");
                     }
             }
 
@@ -21,15 +21,28 @@ $(document).ready(function ()
 
 $(document).on("click", "#saveCfg", function()
     {
-        var newConfig;
+		var newConfig = new Object();
+		var toBoolean = function(string)
+			{
+				switch(string.toLowerCase())
+					{
+						case "true": return true;
+						case "false":  return false;
+						default: return string;
+					}
+			}
 
+		$(".paramName").each(function ()
+			{
+				newConfig[this.value] = toBoolean($("#" + this.value + "_value").val());
+			});
 
-        alert ("dobe"); return;
+		$("#status").html("Saving ...");
 
         $.ajax(
             {
                 type: "POST",
-                data: newConfig,
+                data: JSON.stringify(newConfig),
                 contentType: "text/plain; charset=UTF-8",
                 url : "/radpf/cfg",
                 success: function (response)
@@ -38,7 +51,7 @@ $(document).on("click", "#saveCfg", function()
                     },
                 error: function ()
                     {
-                        alert("Error of saving cfg");
+						$("#status").html("Error of saving cfg");
                     }
             });
     });
